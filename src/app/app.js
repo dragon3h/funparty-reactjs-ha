68c8c412703fa4b6627b42cbb9f524f8ac317e39
dragon3hgmx <dragon3h@gmx.com>
2020-08-11 22:37:48 -0400

    added structure

---
+++ src/app/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const app = () => {
+  return (
+      <div>
+        Hi Romr!
+      </div>
+  );
+};
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-const bodyElement = document.querySelector('body');
-bodyElement.textContent = 'Hello Romr!';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app/app';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
